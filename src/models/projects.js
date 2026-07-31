44ab3624c8b1b3cbc9fbac8ae0dc4219e06b09b7
Franco Brunini <valentinobruni@gmail.com>
2026-07-31 19:21:07 -0300

    c

--- src/models/projects.js
+++ src/models/projects.js
@@ -21,7 +21,7 @@ const getAllProjects = async () => {
 
 const createProject = async (title, description, location, date, organizationId) => {
     const query = `
-      INSERT INTO project (title, description, location, date, organization_id)
+      INSERT INTO projects (title, description, location, date, organization_id)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING project_id;
     `;
